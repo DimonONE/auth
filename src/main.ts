ed0988f5486a6f60e8b6642034043e295158b91d
Dimon <dmezey@mail.ua>
2022-06-15 17:42:40 +0300

    del .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
 
-config();
 const { PORT } = process.env;
 
 async function bootstrap() {
