8b63d093a08364ad1eef548bbbab321b3f0a2027
Dimon <dmezey@mail.ua>
2022-06-15 17:36:08 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors();
   console.log('PORT => ', PORT);
   await app.listen(PORT || 3000);
 }
