195bc085706797daa314cca1152b0683de53307c
Dimon <dmezey@mail.ua>
2022-06-16 16:12:07 +0300

    test

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -13,22 +13,22 @@ exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
 const app_service_1 = require("./app.service");
 let AppController = class AppController {
-    appService;
     constructor(appService) {
         this.appService = appService;
     }
-    getHello() {
-        return this.appService.getHello();
+    getProducts() {
+        return this.appService.getProducts();
     }
 };
 __decorate([
-    (0, common_1.Get)(),
+    (0, common_1.Get)('/products'),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
-    __metadata("design:returntype", String)
-], AppController.prototype, "getHello", null);
+    __metadata("design:returntype", void 0)
+], AppController.prototype, "getProducts", null);
 AppController = __decorate([
     (0, common_1.Controller)(),
     __metadata("design:paramtypes", [app_service_1.AppService])
 ], AppController);
 exports.AppController = AppController;
+//# sourceMappingURL=app.controller.js.map
